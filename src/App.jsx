0df620b3104539984fd85899c3af16482c70ab96
Fardus Ahmed <fardus11@gmail.com>
2023-08-07 14:50:57 -0400

    api function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
+    async function callApi() {
+        const options = {
+            method: "POST",
+            headers: {
+                "content-type": "application/x-www-form-urlencoded",
+                "X-RapidAPI-Key":
+                    "96afb637f6msh22e9497d049080ep10a52ajsna3c7e7fe064e",
+                "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
+            },
+            body: new URLSearchParams({
+                source_language: [source],
+                target_language: [target],
+                text: [inputValue],
+            }),
+        };
+    }
 
-  return (
-    <>
-      
-    </>
-  )
+    return <>
+    
+    </>;
 }
 
-export default App
+export default App;
